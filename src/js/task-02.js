8a8e1003aeef84ed1f6539f757caf33f35a34856
MarianaDobrivska <dobrivska.marry@gmail.com>
2022-10-30 15:55:07 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientList = document.querySelector("#ingredients");
+
+const ingredientsEl = [];
+for (const ingr of ingredients) {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingr;
+  ingredientItem.classList.add("item");
+  ingredientsEl.push(ingredientItem);
+}
+ingredientList.append(...ingredientsEl);
