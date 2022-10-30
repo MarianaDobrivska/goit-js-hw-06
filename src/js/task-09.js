f4e5a3549c8e15795004e4ccb799dc516352c829
MarianaDobrivska <dobrivska.marry@gmail.com>
2022-10-30 19:20:19 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+}
