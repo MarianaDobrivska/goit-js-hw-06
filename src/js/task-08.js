b3eef0136a81038b68216d4f6669eb2e194eff07
MarianaDobrivska <dobrivska.marry@gmail.com>
2022-10-30 19:09:18 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -6,12 +6,14 @@ function onFormSubmit(event) {
   event.preventDefault();
   const formData = event.currentTarget.elements;
   const userData = {};
+
   for (let i = 0; i < formData.length - 1; i++) {
     if (formData[i].value === "") {
       return alert("Усі поля повинні бути заповнені!");
     }
     userData[formData[i].name] = formData[i].value;
   }
+
   console.log(userData);
   event.currentTarget.reset();
   return userData;
