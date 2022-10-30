549caf1dc787aa4a0a6e68febc2b105b3a4ed81a
MarianaDobrivska <dobrivska.marry@gmail.com>
2022-10-30 19:34:50 +0200

    remove excessive files

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector("#font-size-control");
-const textEl = document.querySelector("#text");
-
-inputEl.addEventListener("input", getChangedFontSize);
-
-function getChangedFontSize(event) {
-  textEl.style.fontSize = event.currentTarget.value + "px";
-}
